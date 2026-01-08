cfbe9f72a722c878d5ade87b157aa9adfc1299a8
Khushi Bhati <khushibhati2413@gmail.com>
2026-01-08 12:55:54 +0530

    Update Patierntsidebar.jsx

--- doctor-project/src/components/Patientmodel/Patierntsidebar.jsx
+++ doctor-project/src/components/Patientmodel/Patierntsidebar.jsx
@@ -76,7 +76,7 @@ const PatientSidebar = () => {
                             setActiveDropdown(activeDropdown === "patient" ? "" : "patient")
                         }
                     >
-                        <a href="#">
+                        <a >
 
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -129,7 +129,7 @@ const PatientSidebar = () => {
                         }
                     >
 
-                        <a href="#">
+                        <a >
                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                                 <rect x={3} y={3} width={7} height={7} />
                                 <rect x={14} y={3} width={7} height={7} />
@@ -161,7 +161,7 @@ const PatientSidebar = () => {
                     </li>
 
                     <li className={active === "appointments" ? "sidebar-menu-item active" : "sidebar-menu-item"} onClick={() => setactive(active === "appointments" ? "" : "appointments")}>
-                        <a href="#">
+                        <a >
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                                 <rect x={3} y={4} width={18} height={18} rx={2} ry={2} />
                                 <line x1={16} y1={2} x2={16} y2={6} />
@@ -172,7 +172,7 @@ const PatientSidebar = () => {
                         </a>
                     </li>
                     <li className={active === "settings" ? "sidebar-menu-item active" : "sidebar-menu-item"} onClick={() => setactive(active === "settings" ? "" : "settings")}>
-                        <a href="#">
+                        <a >
                             <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round">
                                 <circle cx={12} cy={12} r={3} />
                                 <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z">
