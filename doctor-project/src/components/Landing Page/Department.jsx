4d3c7442760327fb1bb94d882b90005fd1894527
Ankit Kumar Singh <103576635+ANKIT-SINGH-870075-KUMAR@users.noreply.github.com>
2026-01-08 12:36:27 +0530

    Add useEffect import to Department component

--- doctor-project/src/components/Landing Page/Department.jsx
+++ doctor-project/src/components/Landing Page/Department.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./../../styles/Department.css";
 import cardiologist from "./../../images/cardiologist.webp";
 import surgeon from "./../../images/general-surgeon.webp";
