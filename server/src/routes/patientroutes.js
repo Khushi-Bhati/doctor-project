1ca29aafb78141f0f7f49a2c0f6a85bae6537185
Khushi Bhati <khushibhati2413@gmail.com>
2026-01-08 16:27:42 +0530

    Update patientroutes.js

--- server/src/routes/patientroutes.js
+++ server/src/routes/patientroutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addPatientController, getPatientController, updatePatientimgController, updatePatientController, getaDoctorslist, getDoctorprofileByDoctorid, } from "../controllers/PatientController.js"
+import { addPatientController, getPatientController, updatePatientController, getaDoctorslist, getDoctorprofileByDoctorid, } from "../controllers/PatientController.js"
 import { upload } from "../middlewares/multer.js"
 
 
@@ -8,8 +8,6 @@ const patientrouter = express.Router()
 patientrouter.post("/addpatient",upload.fields([{name:"profileImage",maxCount:1},{name:"healthinsurance",maxCount:1}]),addPatientController)
 patientrouter.get("/getpatient/:id",getPatientController)
 patientrouter.patch("/updatepatient/:userID",updatePatientController)
-patientrouter.patch("/updatepatientimg/:userID",upload.fields([{name:"profileImage",maxCount:1}]),updatePatientimgController)
-
 patientrouter.get("/getdoctorslist",getaDoctorslist)
 patientrouter.get("/viewProfileBydoctorid/:id",getDoctorprofileByDoctorid)
 
