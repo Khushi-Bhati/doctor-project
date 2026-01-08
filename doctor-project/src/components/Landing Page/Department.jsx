0af845d2530e8104aba6ffb23f5791bd4f737404
Ankit Kumar Singh <103576635+ANKIT-SINGH-870075-KUMAR@users.noreply.github.com>
2026-01-08 12:30:33 +0530

    Add useState import to Department component

--- doctor-project/src/components/Landing Page/Department.jsx
+++ doctor-project/src/components/Landing Page/Department.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./../../styles/Department.css";
 import cardiologist from "./../../images/cardiologist.webp";
 import surgeon from "./../../images/general-surgeon.webp";
