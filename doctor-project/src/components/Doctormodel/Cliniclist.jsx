3e1e9ae12e3673204b832d5fa24f5967d1a86632
Khushi Bhati <khushibhati2413@gmail.com>
2026-01-11 13:49:25 +0530

    Update Cliniclist.jsx

--- doctor-project/src/components/Doctormodel/Cliniclist.jsx
+++ doctor-project/src/components/Doctormodel/Cliniclist.jsx
@@ -24,7 +24,7 @@ const getCliniclist = async () => {
   try {
     setLoading(true);
 
-    const query = `${process.env.REACT_APP_API_URL}Hospital/doctor/getclinicslist?page=${currentpage}&limit=${limit}&_=${Date.now()}`;
+    const query = `${process.env.REACT_APP_API_URL}/Hospital/doctor/getclinicslist?page=${currentpage}&limit=${limit}&_=${Date.now()}`;
 
     const response = await axios.get(query);
 
